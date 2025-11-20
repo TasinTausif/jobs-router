65507d9a42917030e9427ad7b981bd34ebfdb3cc
Tasin-Tausif <tasin@magnetismtech.com>
2025-11-20 17:21:29 +0600

    Help Section WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,57 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  NavLink,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider
+} from 'react-router-dom'
+
+// Pages
+import Home from "./pages/Home.jsx"
+import About from "./pages/About.jsx"
+
+// Layouts
+import RootLayout from './layouts/RootLayout.jsx';
+import HelpLayout from './layouts/HelpLayout.jsx'
+
+const route = createBrowserRouter(
+  createRoutesFromElements(
+    // The element in parent will act as a layout for the child elements
+    <Route path='/' element={<RootLayout/>}>
+      <Route index element={<Home />} />{/*By mentioning index inside a path, it is the index of the path related to the parent */}
+      <Route path="about" element={<About />} />
+      <Route path='help' element={<HelpLayout />}>
+        <Route path='faq'/>  
+        <Route path='contact'/>  
+      </Route>
+    </Route>
+  )
+)
+
 export default function App() {
   return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
+
+    // Old Approach
+    // <BrowserRouter>
+    //   <header>
+    //     <nav>
+    //       <h1>Job Portal</h1>
+    //       {/* Link or Navlink finds if matched route is found. Then in the Route, it swaps current data with the element */}
+    //       <NavLink to='/'>Home</NavLink>
+    //       <NavLink to='about'>About</NavLink>{/* In the NavLink, it adds some additional features like adding a class named active automatically that is not available in the Link */}
+    //     </nav>
+    //   </header>
+    //   <main>
+    //     <Routes>
+    //       <Route index element={<Home />} />{/* Since it's the index page or root, here index can be used instead of mentioning the path*/}
+    //       <Route path="about" element={<About />} />
+    //     </Routes>
+    //   </main>
+    // </BrowserRouter>
+
+    // In the first approach, BrowserRouter was created from before and Routes were passed individually. But in this approach, RouteProvider takes router param that takes a self created BrowserRouter
+    <RouterProvider router={route}/>
   );
 }
---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,19 @@
+import { NavLink, Outlet } from "react-router-dom"
+
+export default function RootLayout() {
+    return (
+        <div className="root-layout">
+            <header>
+                <nav>
+                    <h1>Job Portal</h1>
+                    <NavLink to='/'>Home</NavLink>
+                    <NavLink to='about'>About</NavLink>
+                </nav>
+            </header>
+            <main>
+                {/* Outlet matches the parent route's route name with the child's route name and renders the content. Basically if current route is a parent route and it's nested routes content will be showed*/}
+                <Outlet/>
+            </main>
+        </div>
+    )
+}
